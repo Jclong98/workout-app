8e61bb43debba9f219960cfa5beeb78d8494f410
Jacob Long <jclong98@gmail.com>
2022-06-13 14:41:53 -0700

    move attribution

--- components/Attribution.vue
+++ components/Attribution.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="fixed right-2 bottom-0">
+  <div class="fixed left-2 bottom-0">
     <a
       href="https://github.com/jclong98/workout-app"
-      class="inline-flex items-center justify-end w-full font-bold gap-1"
+      class="inline-flex items-center w-full font-bold gap-2"
     >
+      <i class="i-mdi-github text-3xl"></i>
       Source
-      <i class="i-mdi-github text-4xl"></i>
     </a>
 
-    <a href="https://jacoblong.dev" class="mb-2 block w-full text-right">
-      Created by <span class="font-bold">Jacob Long</span>
+    <a href="https://jacoblong.dev" class="mb-2 block w-full">
       <img
-        class="ml-2 inline-block h-8 w-8 rounded-full"
+        class="inline-block h-8 w-8 rounded-full"
         src="https://avatars.githubusercontent.com/u/36164786?s=48&v=4"
         alt="my github profile pic"
       />
+      Created by <span class="font-bold">Jacob Long</span>
     </a>
   </div>
 </template>
