cce93579b3261f492f8a4df6298c64f140cb3766
Jacob Long <jclong98@gmail.com>
2022-06-14 20:39:01 -0700

    feat: page transitions

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="max-w-[400px] mx-auto border-l border-r border-gray/50 h-screen">
-    <slot></slot>
+    <Transition name="page">
+      <slot></slot>
+    </Transition>
+
     <Attribution />
   </div>
 </template>
@@ -9,5 +12,14 @@
 html,
 body {
   @apply bg-gray-100;
+}
+
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.2s ease;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
 }
 </style>
