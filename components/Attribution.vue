795f36b9ffcb406d95d2eb0bbdef2b7762704e43
Jacob Long <jclong98@gmail.com>
2022-11-14 14:44:54 -0700

    fix: attribution styles

--- components/Attribution.vue
+++ components/Attribution.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="fixed left-2 bottom-0">
+  <div class="flex gap-8 py-4 justify-center md:(fixed left-0 bottom-0 flex-col gap-2 px-4)">
     <a
       href="https://github.com/jclong98/workout-app"
-      class="inline-flex items-center w-full font-bold gap-2"
+      class="inline-flex items-center font-bold gap-2"
     >
       <i class="i-mdi-github text-3xl"></i>
       Source
     </a>
 
-    <a href="https://jacoblong.dev" class="mb-2 block w-full">
+    <a href="https://jacoblong.dev" class="inline-flex items-center gap-2 md:(flex-row-reverse)">
+      <span>Created by <span class="font-bold">Jacob Long</span></span>
       <img
         class="inline-block h-8 w-8 rounded-full"
         src="https://avatars.githubusercontent.com/u/36164786?s=48&v=4"
         alt="my github profile pic"
       />
-      Created by <span class="font-bold">Jacob Long</span>
     </a>
   </div>
 </template>
